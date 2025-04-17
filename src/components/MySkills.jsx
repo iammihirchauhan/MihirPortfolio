adebb80e51cadfe752119526882a48b3d8d6e536
iammihirchauhan <chauhanmihirmihir@gmail.com>
2025-04-17 17:41:18 +0530

    css styling

--- src/components/MySkills.jsx
+++ src/components/MySkills.jsx
@@ -1,4 +1,5 @@
 import "./MySkills.css";
+import tick_light from "../assets/checkmark-light.svg";
 import tick_dark from "../assets/checkmark-dark.svg";
 import { MySkillsData } from "../MySkillsData";
 
@@ -7,19 +8,19 @@ export const MySkills = () => {
     <section id="skills" className="skill-container">
       <h1 className="skill-title">Skills</h1>
       <div className="skill-list">
-        <MySkillsData src={tick_dark} skills="REACT" />
+        <MySkillsData src={tick_light} skills="REACT" />
       </div>
       <hr />
       <div className="skill-list">
-        <MySkillsData src={tick_dark}  skills="JAVASCRIPT" />
+        <MySkillsData src={tick_light}  skills="JAVASCRIPT" />
       </div>
       <hr />
       <div className="skill-list">
-        <MySkillsData src={tick_dark} skills="CSS" />
+        <MySkillsData src={tick_light} skills="CSS" />
       </div>
       <hr />
       <div className="skill-list">
-        <MySkillsData src={tick_dark} skills="HTML" />
+        <MySkillsData src={tick_light} skills="HTML" />
       </div>
     </section>
   );
